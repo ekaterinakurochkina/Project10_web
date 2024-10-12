30ba2c6c4bb7cfbe1b6b3c140027cbf3fba8f97c
Katerina Kurochkina <katifedr@mail.ru>
2024-10-12 14:25:32 +0500

    итог

--- src/web.py
+++ src/web.py
@@ -3,20 +3,32 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 
 # Для начала определим настройки запуска
-hostName = "localhost" # Адрес для доступа по сети
-serverPort = 8080 # Порт для доступа по сети
+hostName = "localhost"  # Адрес для доступа по сети
+serverPort = 8080  # Порт для доступа по сети
+
 
 class MyServer(BaseHTTPRequestHandler):
     """
-        Специальный класс, который отвечает за
-        обработку входящих запросов от клиентов
+    Специальный класс, который отвечает за
+    обработку входящих запросов от клиентов
     """
+
     def do_GET(self):
-        """ Метод для обработки входящих GET-запросов """
-        self.send_response(200) # Отправка кода ответа
-        self.send_header("Content-type", "application/json") # Отправка типа данных, который будет передаваться
-        self.end_headers() # Завершение формирования заголовков ответа
-        self.wfile.write(bytes("{'message': 'OK'}", "utf-8")) # Тело ответа
+        """Метод для обработки входящих GET-запросов"""
+        self.send_response(200)  # Отправка кода ответа
+        self.send_header(
+            "Content-type", "application/json"
+        )  # Отправка типа данных, который будет передаваться
+        self.end_headers()  # Завершение формирования заголовков ответа
+        self.wfile.write(bytes("{'message': 'OK'}", "utf-8"))  # Тело ответа
+
+    def do_POST(self):
+        content_length = int(self.headers["Content-Length"])
+        post_data = self.rfile.read(content_length)
+
+        response = f"Received POST data: {post_data.decode('utf-8')}"
+        print(response)
+
 
 if __name__ == "__main__":
     # Инициализация веб-сервера, который будет по заданным параметрах в сети
