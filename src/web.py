e9eb935d464de514650fdb6f2a97c98aca3297cf
Katerina Kurochkina <katifedr@mail.ru>
2024-10-12 14:30:07 +0500

    итог

--- src/web.py
+++ src/web.py
@@ -17,7 +17,7 @@ class MyServer(BaseHTTPRequestHandler):
         """Метод для обработки входящих GET-запросов"""
         self.send_response(200)  # Отправка кода ответа
         self.send_header(
-            "Content-type", "application/json"
+            "Content-type", "text/html"
         )  # Отправка типа данных, который будет передаваться
         self.end_headers()  # Завершение формирования заголовков ответа
         self.wfile.write(bytes("{'message': 'OK'}", "utf-8"))  # Тело ответа
